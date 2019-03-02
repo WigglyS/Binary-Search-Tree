b8fdcb87e3cada64b298b77ce3d27c07a15eca0b
Travis Bodine <trabodin@uat.edu>
2019-03-02 12:06:36 -0700

    Added FInd Max and Min functions

--- Binary Search Tree/Binary Search Tree/Classes.h
+++ Binary Search Tree/Binary Search Tree/Classes.h
@@ -9,16 +9,19 @@ class Node
 {
 public:
 	Node(T newData) { data = newData; }
-	Node* GetLeft() { return Left; }
-	void SetLeft(Node* prev) { Left = prev; }
-	Node* GetRight() { return Right; }
-	void SetRight(Node* next) { Right = next; }
+	Node<T>* GetLeft() { return Left; }
+	void SetLeft(Node<T>* left) { Left = left; }
+	Node<T>* GetRight() { return Right; }
+	void SetRight(Node<T>* right) { Right = right; }
+	Node<T>* GetParent() { return Parent; }
+	void SetParent(Node<T>* parent) { Parent = parent; }
 
 	T Getdata() { return data; };
 	void SetData(T d) { data = d; };
 private:
-	Node* Left = nullptr;
-	Node* Right = nullptr;
+	Node<T>* Left = nullptr;
+	Node<T>* Right = nullptr;
+	Node<T>* Parent = nullptr;
 	T data;
 };
 
@@ -41,6 +44,7 @@ public:
 			RecursiveInsert(temp);
 		}
 	}
+
 	void RecursiveInsert(Node<T>* pointer, T data) {
 		
 		if (data == pointer->Getdata()) {
@@ -49,23 +53,27 @@ public:
 		}
 		if (data > pointer->GetData()) {
 			if (pointer->GetRight() == NULL) {
-				pointer->SetRight(new Node<T>(data));
+				Node<T>* temp = new Node<T>(data);
+				pointer->SetRight(temp);
+				temp->SetParent(pointer);
 				return;
 			}
 			else {
 
-				RecursiveInsert(pointer->GetRight());
+				RecursiveInsert(pointer->GetRight(), data);
 			}
 
 		}
 		else if (data < pointer->GetData()) {
 			if (pointer->GetLeft() == NULL) {
-				pointer->SetLeft(new Node<T>(data));
+				Node<T>* temp = new Node<T>(data);
+				pointer->SetLeft(temp);
+				temp->SetParent(pointer);
 				return;
 			}
 			else
 			{
-				RecursiveInsert(pointer->GetLeft());
+				RecursiveInsert(pointer->GetLeft(), data);
 			}
 		}
 	
@@ -83,8 +91,39 @@ public:
 	}
 
 	Node<T>* Find(T data) {
-
+		if (Root == nullptr) {
+			cout << "There is no data in the List" << endl;
+			return nullptr;
+		}
+		Node<T>* SearchPointer = Root;
+		return RecusiveFind(SearchPointer, data);
 	}
+
+	Node<T>* RecusiveFind(Node<T>* SearchPointer, T data) {
+		if (data == SearchPointer->Getdata()) {
+			return SearchPointer;
+		}
+		if (data > SearchPointer->Getdata()) {
+			if (SearchPointer->GetRight() == nullptr) {
+				cout << "That value is not search tree" << endl;
+				return nullptr;
+			}
+			else {
+				RecusriveFind(SearchPointer->GetRight(), data);
+			}
+		}
+		if (data < SearchPointer->Getdata()) {
+			if (SearchPointer->GetLeft() == nullptr) {
+				cout << "That value is not search tree" << endl;
+				return nullptr;
+			}
+			else {
+				RecursiveFind(SearchPointer->GetLeft(), data);
+			}
+		}
+	}
+
+
 
 	void Traverse() {
 		//recursively call left and right for all the nodes
@@ -93,9 +132,19 @@ public:
 	Node<T>* Maximum(Node<T>* startingPoint)
 	{
 		//search for the farthest node to the right 
+		Node<T>* IterativePointer;
+		while (IterativePointer->GetRight() != nullptr) {
+			IterativePointer = IterativePointer->GetRight();
+		}
+		return IterativePointer;	
 	}
 	Node<T>* Minimum(Node<T>* startingPoint)
 	{
-
+		//search for the farthest node to the left
+		Node<T>* IterativePointer;
+		while (IterativePointer->GetLeft() != nullptr) {
+			IterativePointer = IterativePointer->GetLeft();
+		}
+		return IterativePointer;
 	}
 };
